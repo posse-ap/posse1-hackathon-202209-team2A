af9d5abf9c6f7f41c785193855c49c34d0550094
kazuki-naka <kazunaka.0077@gmail.com>
2022-09-07 15:37:51 +0900

    前日分のイベント抽出を途中まで

--- src/cruds/Notification.php
+++ src/cruds/Notification.php
@@ -19,7 +19,7 @@ class Notification
   }
 
   public function before_attendance_event() {
-    $stmt = $this->db ->prepare("SELECT name,details,start_at FROM events WHERE start_at -1=now()");
+    $stmt = $this->db ->prepare("SELECT name,detail,start_at FROM events WHERE DATE_SUB(CURRENT_DATE, INTERVAL 1 DAY)");
     $stmt -> execute();
     return $stmt -> fetchAll();
   }
--- src/cruds/domains/Email.php
+++ src/cruds/domains/Email.php
@@ -1,30 +1,30 @@
 <?
-  namespace cruds\domains;
-  require_once('../../config.php');
-  mb_language('ja');
-  mb_internal_encoding('UTF-8');
+namespace cruds\domains;
+require_once('../../config.php');
+mb_language('ja');
+mb_internal_encoding('UTF-8');
 
 
 
 class Email
 {
-  public function mail_send($to) {
-    $subject = "イベント前日通知";
-    $body = "本文";
-    $headers = ["From"=>"system@posse-ap.com", "Content-Type"=>"text/plain; charset=UTF-8", "Content-Transfer-Encoding"=>"8bit"];
+    public function mail_send($to) {
+        $subject = "イベント前日通知";
+        $body = "本文";
+        $headers = ["From"=>"system@posse-ap.com", "Content-Type"=>"text/plain; charset=UTF-8", "Content-Transfer-Encoding"=>"8bit"];
+        
+        $date = "2021年08月01日（日） 21:00~23:00";
+        $event = "縦モク";
+        $body = <<<EOT
+        
+        ${date}に${event}を開催します。
+        参加／不参加の回答をお願いします。
+        
+        http://localhost/auth/login
+        EOT;
+        
+        mb_send_mail($to, $subject, $body, $headers);
+        echo "メールを送信しました";
+    }
     
-    $date = "2021年08月01日（日） 21:00~23:00";
-    $event = "縦モク";
-    $body = <<<EOT
-    
-    ${date}に${event}を開催します。
-    参加／不参加の回答をお願いします。
-    
-    http://localhost/auth/login
-    EOT;
-    
-    mb_send_mail($to, $subject, $body, $headers);
-    echo "メールを送信しました";
-  }
-  
 }
