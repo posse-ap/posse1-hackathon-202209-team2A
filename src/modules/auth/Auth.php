2d5c8fb946fbb6084a145b858c563ff1d1aef912
fukuba-shuma <calculus.fukuba@gmail.com>
2022-09-07 13:00:21 +0900

    reset password

--- src/modules/auth/Auth.php
+++ src/modules/auth/Auth.php
@@ -39,4 +39,13 @@ class Auth
         header("Location: http://" . $_SERVER['HTTP_HOST'] . "/auth/login/" . $this->address);
         exit();
     }
+
+    public function reset_password($email, $current_password, $new_password)
+    {
+        $user = $this->cruds->get_user($email);
+        $current_user_password = $user['hashed_password'];
+        if (sha1($current_password) === $current_user_password){
+            return $this->cruds->update_user_password($email, $new_password);
+        }
+    }
 }
