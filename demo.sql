19266807b04c676dc35357ba8f948d73c63e9705
フルヤミウ <miyamiu19642002@gmail.com>
2022-09-08 19:55:58 +0900

    ファイル追加

--- demo.sql
+++ demo.sql
@@ -6,8 +6,8 @@ WHERE start_at > now()
 AND is_attendance = 2 ORDER BY event_id;
 
 --SQLで参加者一覧を出力する #23
-select name,start_at,username from events 
-join event_attendance on events.id = event_attendance.event_id 
-join users on users.id = event_attendance.user_id 
-where start_at >= now() 
-and is_attendance = 1;
+SELECT event_id,events.name,username,start_at FROM users 
+INNER JOIN event_attendance ON users.id = user_id
+INNER JOIN events ON events.id = event_id
+WHERE start_at > now()
+AND is_attendance = 1 ORDER BY event_id;
