709896878bb2b415673edfe62cd898d5de88e085
fukuba-shuma <calculus.fukuba@gmail.com>
2022-09-08 10:34:53 +0900

    disabled by is_attendance

--- src/attendance.php
+++ src/attendance.php
@@ -18,7 +18,7 @@ if (!isset($event_id)) {
     exit();
 }
 
-$event = $crud->read_event($event_id);
+$event = $crud->read_event($event_id, $user_id);
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_REQUEST['is_attendance'])) {
@@ -49,61 +49,16 @@ include dirname(__FILE__) . '/component/header.php';
 </header>
 
 <main class="bg-gray-100">
-    <div class="w-full mx-auto p-5">
-        <div id="filter" class="mb-8">
-            <h2 class="text-sm font-bold mb-3">フィルター</h2>
-            <div id="attendance_button_container" class="flex">
-                <a href="index.php" class="px-3 py-2 text-md font-bold mr-2 rounded-md shadow-md bg-white">全て</a>
-                <a href="index.php?is_attendance=1" class="px-3 py-2 text-md font-bold mr-2 rounded-md shadow-md bg-white">参加</a>
-                <a href="index.php?is_attendance=0" class="px-3 py-2 text-md font-bold mr-2 rounded-md shadow-md bg-white">不参加</a>
-                <a href="index.php?is_answered=1" class="px-3 py-2 text-md font-bold mr-2 rounded-md shadow-md bg-white">未回答</a>
-            </div>
+    <div id="events-list">
+        <div class="flex justify-between items-center mb-3">
+            <h2 class="text-sm font-bold">一覧</h2>
         </div>
-
-
-
-
-        <div id="events-list">
-            <div class="flex justify-between items-center mb-3">
-                <h2 class="text-sm font-bold">一覧</h2>
-            </div>
-            <?php
-            $start_date = strtotime($event['start_at']);
-            $end_date = strtotime($event['end_at']);
-            $day_of_week = Utils::get_day_of_week(date("w", $start_date));
-            ?>
-            <div class="modal-open bg-white mb-3 p-4 flex justify-between rounded-md shadow-md cursor-pointer" id="event-<?php echo $event['id']; ?>">
-                <div>
-                    <h3 class="font-bold text-lg mb-2"><?php echo $event['name'] ?></h3>
-                    <p><?php echo date("Y年m月d日（${day_of_week}）", $start_date); ?></p>
-                    <p class="text-xs text-gray-600">
-                        <?php echo date("H:i", $start_date) . "~" . date("H:i", $end_date); ?>
-                    </p>
-                </div>
-                <div class="flex flex-col justify-between text-right">
-                    <div>
-                        <?php if ($event['id'] % 3 === 1) : ?>
-                            <!--
-                  <p class="text-sm font-bold text-yellow-400">未回答</p>
-                  <p class="text-xs text-yellow-400">期限 <?php echo date("m月d日", strtotime('-3 day', $end_date)); ?></p>
-                  -->
-                        <?php elseif ($event['id'] % 3 === 2) : ?>
-                            <!--
-                  <p class="text-sm font-bold text-gray-300">不参加</p>
-                  -->
-                        <?php else : ?>
-                            <!--
-                  <p class="text-sm font-bold text-green-400">参加</p>
-                  -->
-                        <?php endif; ?>
-                    </div>
-                    <p class="text-sm"><span class="text-xl"><?= count($event['attendance_users']) ?></span>人参加 ></p>
-                    <?php foreach ($event['attendance_users'] as $attendance) :  ?>
-                        <div><?= $attendance['username'] ?></div>
-                    <?php endforeach ?>
-                </div>
-            </div>
-
+        <?php
+        $start_date = strtotime($event['start_at']);
+        $end_date = strtotime($event['end_at']);
+        $day_of_week = Utils::get_day_of_week(date("w", $start_date));
+        ?>
+        <div class="modal-open bg-white mb-3 p-4 flex justify-between rounded-md shadow-md cursor-pointer" id="event-<?php echo $event['id']; ?>">
             <div>
                 <h3 class="font-bold text-lg mb-2"><?php echo $event['name'] ?></h3>
                 <p><?php echo date("Y年m月d日（${day_of_week}）", $start_date); ?></p>
@@ -133,13 +88,48 @@ include dirname(__FILE__) . '/component/header.php';
                     <div><?= $attendance['username'] ?></div>
                 <?php endforeach ?>
             </div>
-            <form action="" method="POST">
-                <input type="hidden" name="event_id" value="<?= $event['id'] ?>">
-                <label for="attendance_radio"><input id="attendance_radio" type="radio" value="1" name="is_attendance">参加</label>
-                <label for="unattendance_radio"><input id="unattendance_radio" type="radio" value="0" name="is_attendance">不参加</label>
-                <br>
-                <input type="submit" value="登録" class="text-white bg-blue-400 px-4 py-2 rounded-3xl bg-gradient-to-r from-blue-600 to-blue-200">
-            </form>
         </div>
+
+        <div>
+            <h3 class="font-bold text-lg mb-2"><?php echo $event['name'] ?></h3>
+            <p><?php echo date("Y年m月d日（${day_of_week}）", $start_date); ?></p>
+            <p class="text-xs text-gray-600">
+                <?php echo date("H:i", $start_date) . "~" . date("H:i", $end_date); ?>
+            </p>
+        </div>
+        <div class="flex flex-col justify-between text-right">
+            <div>
+                <?php if ($event['id'] % 3 === 1) : ?>
+                    <!--
+                  <p class="text-sm font-bold text-yellow-400">未回答</p>
+                  <p class="text-xs text-yellow-400">期限 <?php echo date("m月d日", strtotime('-3 day', $end_date)); ?></p>
+                  -->
+                <?php elseif ($event['id'] % 3 === 2) : ?>
+                    <!--
+                  <p class="text-sm font-bold text-gray-300">不参加</p>
+                  -->
+                <?php else : ?>
+                    <!--
+                  <p class="text-sm font-bold text-green-400">参加</p>
+                  -->
+                <?php endif; ?>
+            </div>
+            <p class="text-sm"><span class="text-xl"><?= count($event['attendance_users']) ?></span>人参加 ></p>
+            <?php foreach ($event['attendance_users'] as $attendance) :  ?>
+                <div><?= $attendance['username'] ?></div>
+            <?php endforeach ?>
+        </div>
+        <form action="" method="POST">
+            <input type="hidden" name="event_id" value="<?= $event['id'] ?>">
+            <label for="attendance_radio"><input id="attendance_radio" type="radio" value="1" name="is_attendance" <?php if ($event['is_attendance']) {
+                echo 'disabled';
+            } ?>>参加</label>
+            <label for="unattendance_radio"><input id="unattendance_radio" type="radio" value="0" name="is_attendance" <?php if (!$event['is_attendance']) {
+                echo 'disabled';
+            } ?>>不参加</label>
+            <br>
+            <input type="submit" value="登録" class="text-white bg-blue-400 px-4 py-2 rounded-3xl bg-gradient-to-r from-blue-600 to-blue-200">
+        </form>
+    </div>
     </div>
 </main>
